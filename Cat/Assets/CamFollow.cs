aa37ca30c030af2ccba6718e0cfca51856700556
ann19951018 <ann19951018@gmail.com>
2022-05-21 09:22:09 +0800

    update

---
+++ Cat/Assets/CamFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+	public Transform MyTarget;
+	public Vector2 myFocus;
+	private Camera myCam;
+	public float mySmoothTime;
+	public Vector3 volocity;
+
+	// Use this for initialization
+	void Start () {
+		myCam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 pos = myCam.WorldToViewportPoint (MyTarget.position);//
+		Vector3 distance = MyTarget.position-myCam.ViewportToWorldPoint(new Vector3 (myFocus.x, myFocus.y, pos.z));
+		Debug.Log (distance);
+		Vector3 moveto = transform.position = transform.position + distance;
+		transform.position = Vector3.SmoothDamp (transform.position, moveto, ref volocity, mySmoothTime);
+ 
+		/*if (MyTarget.transform.position.x > 4.5) //簡單的寫法
+		{
+			this.transform.position = new Vector3 (MyTarget.transform.position.x, this.transform.position.y, this.transform.position.z);
+		} else 
+		{
+		}*/
+	}
+}
---
+++ Cat/Assets/platform.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platform : MonoBehaviour 
+{
+
+	public bool isMoving;
+	//public Transform target;
+	public Transform [] target;
+	public Vector3 velocity;
+	private int index;
+
+	// Use this for initialization
+	void Start () 
+	{
+		isMoving = false;
+		index = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isMoving == true) 
+		{
+			//this.transform.position = Vector3.SmoothDamp(this.transform.position,target.position,ref velocity,2.0f);
+			this.transform.position = Vector3.SmoothDamp(this.transform.position,target[index].position,ref velocity,2.0f);
+			//Debug.Log (velocity);
+			if (Mathf.Abs (velocity.x) < 0.01f) //if(velocity.magnitude < 0.01f)
+			{
+				index = (index + 1) % 2;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D( Collision2D other )
+	{
+		isMoving = true;
+	}
+
+	void OnCollisionStay2D( Collision2D other )
+	{
+		
+	}
+}
